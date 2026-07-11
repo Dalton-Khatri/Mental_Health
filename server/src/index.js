64e32df9e3fbbeefdcd83ec399224503b4e1bbc5
Dalton Khatri <khatridalton1235@gmail.com>
2026-07-11 13:05:50 +0545

    Database update for assessment

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const express = require('express');
 const authRoutes = require('./routes/authRoutes');
 const screeningRoutes = require('./routes/screeningRoutes');
+const assessmentRoutes = require('./routes/assessmentRoutes');
 
 const app = express();
 app.use(cors());
@@ -12,6 +13,7 @@ app.use('/uploads', express.static('uploads')); // so saved audio files can be p
 
 app.use('/api/auth', authRoutes);
 app.use('/api/screenings', screeningRoutes);
+app.use('/api/assessments', assessmentRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
